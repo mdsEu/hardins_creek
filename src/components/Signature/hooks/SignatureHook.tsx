9e2de3ff345afe3b3ccb055e4fb034ae79fa88e5
Fernando Jose Torres Bermudez <fernandojoset@gmail.com>
2022-05-15 22:32:11 -0500

    Update SignatureHook.tsx

--- src/components/Signature/hooks/SignatureHook.tsx
+++ src/components/Signature/hooks/SignatureHook.tsx
@@ -7,7 +7,6 @@ function SignatureHook(store: any) {
     const siganture: any = signatureRef.current;
 
     const img = siganture.toDataURL("image/png");
-    console.log(img)
     const contentType = 'image/png';
     const b64Data = img.replace('data:image/png;base64,','');
     siganture.clear();
