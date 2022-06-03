f1115f923b05857b41b1789ee1364d3c13693252
Fernando Jose Torres Bermudez <fernandojoset@gmail.com>
2022-06-03 18:40:21 -0500

    Revert "change load components from route"
    This reverts commit 240574a0d307636876266ef7e1b3d85913132857.

--- src/components/modal/SignUpModal.tsx
+++ src/components/modal/SignUpModal.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import styles from '../../styles/Modal.module.scss';
-import { IModal } from '../../types';
+import styles from '@/styles/Modal.module.scss';
+import { IModal } from '@/types/index';
 
-type Props = {
+type Props = { 
   modal: IModal,
   noThanks: (bool: boolean) => void,
 }
