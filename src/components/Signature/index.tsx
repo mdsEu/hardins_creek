1e7932e96a23b86a4f44c8a7ae23f686d1840d1a
Juan Pulido <alephart@gmail.com>
2022-06-06 14:00:29 -0500

    fix

--- src/components/Signature/index.tsx
+++ src/components/Signature/index.tsx
@@ -46,10 +46,9 @@ function Signature(props: any) {
           <span className={styles.icon_check}>
             <svg height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/></svg>
           </span>
-          *I agree to let Hardin´s Creek display my signature on their website.
-          {
-            isErrorTerms && <div className={styles.error_terms}></div>
-          }
+          <span className={isErrorTerms ? styles.error_terms : ''}>
+            *I agree to let Hardin´s Creek display my signature on their website.
+          </span>
         </label>
       </div>
       <button className="btn_submit" onClick={saveImg}>
