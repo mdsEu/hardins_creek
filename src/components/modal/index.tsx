2819ce016bd5ba2c29a4c89e22431807b5cbcaf5
Juan Carlos Pulido <72567892+juancpulidos-mds@users.noreply.github.com>
2022-06-08 13:53:40 -0500

    Revert "New modal to Publicity Waiver...."

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,6 +1,5 @@
 import SignatureModal from './SignatureModal';
 import SignUpModal from './SignUpModal';
 import IFrameModal from './IFrameModal';
-import PublicityWaiverModal from './PublicityWaiverModal';
 
-export { SignatureModal, SignUpModal, IFrameModal, PublicityWaiverModal };
+export { SignatureModal, SignUpModal, IFrameModal };
